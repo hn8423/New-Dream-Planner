33c7e54b7b468733357af7561bb2ec709ea1e9fe
김현인 <rolenche@gmail.com>
2022-08-14 01:53:48 +0900

    데이터 베이스 애러 발견

--- new-planner/lib/prisma.js
+++ new-planner/lib/prisma.js
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-// const { PrismaClient } = require('@prisma/client')
+// import { PrismaClient } from "@prisma/client";
+const { PrismaClient } = require("@prisma/client");
 
 Object.defineProperty(exports, "__esModule", { value: true });
 
